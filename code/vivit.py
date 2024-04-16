9e079b65eb678dacc309c39233bc0d5188d38b19
Jonathan Ferrari <jonathanferrari@berkeley.edu>
2024-04-16 11:02:46 -0700

    change params

--- code/vivit.py
+++ code/vivit.py
@@ -11,7 +11,7 @@ classes = list(labels_df.groupby('label').size().sort_values(ascending=False)[:1
 test_files = [f for f in files if labels[int(os.path.basename(f).split('.')[0])] in classes]
 
 
-BATCH_SIZE = 32
+BATCH_SIZE = 16
 AUTO = tf.data.AUTOTUNE
 INPUT_SHAPE = (20, 64, 64, 1)
 # OPTIMIZER
@@ -23,14 +23,14 @@ WEIGHT_DECAY = 1e-5
 EPOCHS = 10
 
 # TUBELET EMBEDDING
-PATCH_SIZE = (8, 8, 8)
+PATCH_SIZE = (8, 8, 4)
 NUM_PATCHES = (INPUT_SHAPE[0] // PATCH_SIZE[0]) ** 2
 
 # ViViT ARCHITECTURE
 LAYER_NORM_EPS = 1e-6
 PROJECTION_DIM = 156
-NUM_HEADS = 10
-NUM_LAYERS = 10
+NUM_HEADS = 16
+NUM_LAYERS = 16
 train_gen = VideoDataGenerator(test_files, batch_size=32)
 
 class TubeletEmbedding(layers.Layer):
@@ -48,6 +48,9 @@ class TubeletEmbedding(layers.Layer):
         projected_patches = self.projection(videos)
         flattened_patches = self.flatten(projected_patches)
         return flattened_patches
+    
+    def build(self, input_shape):
+        return super().build(input_shape)
     
 class PositionalEncoder(layers.Layer):
     def __init__(self, embed_dim, **kwargs):
